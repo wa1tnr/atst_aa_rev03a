0b865aa975e1b4efba1bd0ed9943cbc6f0ddebd2
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-28 17:20:54 +0000

    Raise or Lower dotstar_data - or init 	modified:   dotstar.c

--- dotstar.c
+++ dotstar.c
@@ -2,8 +2,8 @@
 
 #include "driver_init.h"
 
-// During early testing (16:38z 28 July) D13 is
-// substituted for D7 to verify gpio technique.
+// During early testing (17:06z 28 July) D13 is
+// substituted for D6 to verify gpio technique.
 
 // This perforce displaces correct D13 operation,
 // already established.  Correct use commented out.
@@ -13,6 +13,7 @@
 #define PINN D13 // D6
 
 void init_dotstar_gpio(void) {
+    gpio_set_pin_direction(D6, GPIO_DIRECTION_OUT);
     gpio_set_pin_direction(D7, GPIO_DIRECTION_OUT);
 }
 
@@ -46,5 +47,37 @@ void lower_DS_CLOCK(void) {
 	                   false);
 
 	// gpio_set_pin_function(D7,  GPIO_PIN_FUNCTION_OFF);
+}
+
+void raise_DS_DATA(void) {
+	// GPIO on PA20 - D6 - Dotstar Data
+
+	// Set pin direction to output
+	// gpio_set_pin_direction(D6, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(PINN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// gpio_set_pin_function(D6, GPIO_PIN_FUNCTION_OFF);
+}
+
+void lower_DS_DATA(void) {
+	// GPIO on PA20 - D6 - Dotstar Data
+
+	// Set pin direction to output
+	// gpio_set_pin_direction(D6, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(PINN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// gpio_set_pin_function(D6, GPIO_PIN_FUNCTION_OFF);
 }
 
