b2b17fe7cd56d653374441fc1ac2941ed7140173
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-28 00:18:37 +0000

    BLINK LED.  empty loops as timers. Useful program. 	modified:   main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <atmel_start.h>
 #include "thisproject.h" // local include for main.c
+#include "gpio_local.h"
 
 /* Many changes: by wa1tnr, July 2018 */
 
@@ -17,18 +18,29 @@ void uSec(void) {
 
 void short_timer(void) { // human blinkie timescale
     uint32_t on_time  = 2140111222; // it's 2147 something ;)
-    for(on_time = 214011; on_time > 0; on_time--) {
+    for(on_time = 21401; on_time > 0; on_time--) {
         uSec();
     }
 }
 
-void init_act_LED(void) { // PIN_ACTIVITY_LED
-//    gpio_init(PORTA, PIN_ACTIVITY_LED, 1); // PA17
+void vy_short_timer(void) { // human blinkie timescale
+    uint32_t on_time  = 2140111222; // it's 2147 something ;)
+    for(on_time = 344; on_time > 0; on_time--) {
+        uSec();
+    }
 }
 
-
+void long_timer(void) {
+    for(int i =  9; i > 0; i--) {
+        short_timer();
+    }
+}
 
 void blink_LED(void) {
+    raise_D13();
+    short_timer();
+    lower_D13();
+    short_timer();
 /*
  // gpio_write(int port, int pin, int val)
     gpio_write(PORTA, PIN_ACTIVITY_LED, 1); // PA17
@@ -36,6 +48,13 @@ void blink_LED(void) {
     gpio_write(PORTA, PIN_ACTIVITY_LED, 0); // PA17
     short_timer();
 */
+}
+
+void flicker_LED(void) {
+    raise_D13();
+    vy_short_timer();
+    lower_D13();
+    vy_short_timer();
 }
 
 void blinkLEDfast(void) {
@@ -46,23 +65,23 @@ void blinkLEDfast(void) {
 
 void activity_LED_demo(void) {
     init_act_LED();
-    // blinkLEDfast();
-}
-
-int xpmain(void) {
-    // board_init(); // Initialize the SAM system
-    // activity_LED_demo();
-    while(1); // ////////////  trap!  ////////////////
+    blinkLEDfast();
 }
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
-        USB_0_PORT_init(); // redundant?
+        // USB_0_PORT_init(); // redundant?
+
         activity_LED_demo();
+
+        lower_D13();
+        long_timer();
 
 	/* Replace with your application code */
 	while (1) {
+            flicker_LED();
+            long_timer();
 	}
 }
